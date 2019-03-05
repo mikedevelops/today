8aac0a4825bdcc5a76afb2257fb770e3da52e638
Mike Smart <mike.smart@jadu.co.uk>
2019-03-05 21:41:35 +0000

    Sets up the client application

---
+++ packages/journal-client/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <pre>Hello, World!</pre>;
+const root = document.getElementById('root');
+
+ReactDOM.render(<App/>, root);
