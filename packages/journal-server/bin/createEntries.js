de22e8a227cb79cae82584d1e9d38d73dfe5b686
Mike Smart <mike.smart@jadu.co.uk>
2019-05-06 19:25:21 +0100

    Add fixture scripts

---
+++ packages/journal-server/bin/createEntries.js
@@ -0,0 +1,81 @@
+const { loremIpsum } = require('lorem-ipsum');
+const moment = require('moment');
+const meow = require('meow');
+const winston = require('winston');
+const User = require('../src/models/User');
+const Entry = require('../src/models/Entry');
+
+const cli = meow('Create user entries', {
+    flags: {
+        user: {
+            type: 'string',
+        },
+        chance: {
+            type: 'number',
+            defaultValue: 0.5,
+        },
+        db: {
+            type: 'string',
+            defaultValue: 'journal-test',
+        },
+    },
+});
+
+process.env.DB_HOST = `mongodb://localhost/${cli.flags.db}`;
+process.env.DB_NAME = cli.flags.db;
+
+require('../src/services/database');
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.cli(),
+        }),
+    ],
+});
+
+if (cli.flags.user === undefined) {
+    throw new Error('Username required --user');
+}
+
+const start = moment('01-01-2019', 'DD-MM-YYYY');
+const end = moment();
+const entries = [];
+let date = start;
+
+if (start.isAfter(end)) {
+    throw new Error('Start date must be before the end date');
+}
+
+User.findOne({ username: cli.flags.user }, (error, user) => {
+    if (error) {
+        throw new Error(error);
+    }
+
+    if (user === null) {
+        throw new Error(`Cannot find user "${cli.flags.user}"`);
+    }
+
+    Entry.remove({}).then(() => {
+        while(start.isSameOrBefore(end)) {
+            if (Math.random() >= cli.flags.chance) {
+                date.add(1, 'day');
+                continue;
+            }
+
+            entries.push(Entry.create({
+                content: loremIpsum({ count: 5, unit: 'sentences' }),
+                date: date.toDate(),
+                user: user._id,
+            }));
+
+            date.add(1, 'day');
+        }
+
+
+        Promise.all(entries).then(() => {
+            logger.info(`Created ${entries.length} entries for user "${cli.flags.user}"`);
+            process.exit(0);
+        });
+    });
+});
---
+++ packages/journal-server/bin/createUser.js
@@ -0,0 +1,53 @@
+const meow = require('meow');
+const winston = require('winston');
+const User = require('../src/models/User');
+
+const cli = meow('Create user entries', {
+    flags: {
+        user: {
+            type: 'string',
+        },
+        db: {
+            type: 'string',
+            defaultValue: 'journal-test',
+        },
+    },
+});
+
+
+process.env.DB_HOST = `mongodb://localhost/${cli.flags.db}`;
+process.env.DB_NAME = cli.flags.db;
+
+require('../src/services/database');
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.cli(),
+        }),
+    ],
+});
+
+if (cli.flags.user === undefined) {
+    throw new Error('Username required --user');
+}
+
+User.findOne({ username: cli.flags.user }, (error, user) => {
+    if (error) {
+        throw new Error(error);
+    }
+
+    if (user !== null) {
+        logger.info(`User "${user.username}" already exists`);
+        process.exit(0);
+    }
+
+    User.create({ username: cli.flags.user, password: 'Admin123!' }, (error, user) => {
+        if (error !== null) {
+            throw new Error(error);
+        }
+
+        logger.info(`User created "${user.username}"`);
+        process.exit(0);
+    });
+});
