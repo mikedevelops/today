109e8d8d6b5aac7e6efd83c9fff237ecd484dbab
Mike Smart <mike.smart@jadu.co.uk>
2019-06-21 19:54:35 +0100

    Add activity choice type

--- packages/journal-server/src/schema/activity/activity.js
+++ packages/journal-server/src/schema/activity/activity.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const { ACTIVITY_BOOLEAN } = require('./activityBlueprint');
+const { ACTIVITY_BOOLEAN, ACTIVITY_CHOICE } = require('./activityBlueprint');
 
 const activity = new mongoose.Schema({
     icon: { type: String, required: true },
     name: { type: String, required: true },
-    type: { type: String, enum: [ACTIVITY_BOOLEAN], required: true },
+    type: { type: String, enum: [ACTIVITY_BOOLEAN, ACTIVITY_CHOICE], required: true },
     value: { type: mongoose.Schema.Types.Mixed, required: true },
 });
 
--- packages/journal-server/src/schema/activity/activityBlueprint.js
+++ packages/journal-server/src/schema/activity/activityBlueprint.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const ACTIVITY_BOOLEAN = 'ACTIVITY_BOOLEAN';
+const ACTIVITY_CHOICE = 'ACTIVITY_CHOICE';
 
 const activityBlueprintSchema = new mongoose.Schema({
     icon: { type: String, required: true },
     name: { type: String, required: true },
-    type: { type: String, enum: [ACTIVITY_BOOLEAN], required: true },
+    type: { type: String, enum: [ACTIVITY_BOOLEAN, ACTIVITY_CHOICE], required: true },
     defaultValue: { type: mongoose.Schema.Types.Mixed, required: true },
 });
 
